4112b0e6010b1a95ee1581871d3431248548a215
spinalcom <baqui_w@etna-alternance.net>
2019-08-01 11:24:16 +0200

    update in multi

--- circularMenuClass.js
+++ circularMenuClass.js
@@ -1,4 +1,4 @@
-import bimobjService from "spinal-env-viewer-plugin-bimobjectservice";
+// import bimobjService from "spinal-env-viewer-plugin-bimobjectservice";
 import circularMenuVue from "spinal-env-viewer-plugin-circular-menu/circularMenu.vue";
 import Vue from "vue";
 const circularComponentCtor = Vue.extend(circularMenuVue);
@@ -19,52 +19,62 @@ var circularMenu = class circularMenu {
       x: 0,
       y: 0,
     };
-    viewer.clientContainer.addEventListener("click", evt => {
-      this.close();
-      if (evt.target instanceof HTMLCanvasElement) this.onEvt("canvas");
-    });
-    const defaultSingleTapHandler = viewer.clickHandler.handleSingleTap
-    viewer.clickHandler.handleSingleTap = evt => {
-      this.close();
-      if (evt.target instanceof HTMLCanvasElement) {
-        this.evt.xTouch = evt.center.x;
-        this.evt.yTouch = evt.center.y;
-        this.onEvt("canvas");
-      }
-      defaultSingleTapHandler(evt);
+
+    viewer.addEventListener(window.Autodesk.Viewing.TOOLBAR_CREATED_EVENT, eventListener)
+
+    function eventListener() {
+      viewer.removeEventListener(window.Autodesk.Viewing.TOOLBAR_CREATED_EVENT, eventListener)
+
+      viewer.clientContainer.addEventListener("click", evt => {
+        this.close();
+        if (evt.target instanceof HTMLCanvasElement) this.onEvt(
+          "canvas");
+      });
+      const defaultSingleTapHandler = viewer.clickHandler
+        .handleSingleTap
+      viewer.clickHandler.handleSingleTap = evt => {
+        this.close();
+        if (evt.target instanceof HTMLCanvasElement) {
+          this.evt.xTouch = evt.center.x;
+          this.evt.yTouch = evt.center.y;
+          this.onEvt("canvas");
+        }
+        defaultSingleTapHandler(evt);
+      };
+      viewer.clientContainer.addEventListener("DynamicObjectClick",
+        evt => {
+          this.close();
+          try {
+            this.evt.x = event.x;
+            this.evt.y = event.y;
+          } catch (e) {
+            this.evt.x = this.evt.xTouch;
+            this.evt.y = this.evt.yTouch;
+          }
+          this.evt.data = event;
+          this.onEvt("dynamic");
+        });
+      viewer.addEventListener(Autodesk.Viewing.ESCAPE_EVENT, e => {
+        this.close();
+      });
+      viewer.addEventListener(Autodesk.Viewing
+        .AGGREGATE_SELECTION_CHANGED_EVENT,
+        e => {
+          try {
+            this.evt.x = event.x;
+            this.evt.y = event.y;
+          } catch (e) {
+            this.evt.x = this.evt.xTouch;
+            this.evt.y = this.evt.yTouch;
+
+          }
+          this.evt.data = e.selections;
+          this.close();
+          this.onEvt("selected");
+          // this.onSelectionChange.call(this, e)
+        });
     };
 
-    viewer.clientContainer.addEventListener("DynamicObjectClick", evt => {
-      this.close();
-      try {
-        this.evt.x = event.x;
-        this.evt.y = event.y;
-      } catch (e) {
-        this.evt.x = this.evt.xTouch;
-        this.evt.y = this.evt.yTouch;
-      }
-      this.evt.data = event;
-      this.onEvt("dynamic");
-    });
-    viewer.addEventListener(Autodesk.Viewing.ESCAPE_EVENT, e => {
-      this.close();
-    });
-    viewer.addEventListener(Autodesk.Viewing
-      .AGGREGATE_SELECTION_CHANGED_EVENT,
-      e => {
-        try {
-          this.evt.x = event.x;
-          this.evt.y = event.y;
-        } catch (e) {
-          this.evt.x = this.evt.xTouch;
-          this.evt.y = this.evt.yTouch;
-
-        }
-        this.evt.data = e.selections;
-        this.close();
-        this.onEvt("selected");
-        // this.onSelectionChange.call(this, e)
-      });
   }
   onEvt(evtFrom) {
     if (evtFrom === "canvas" || evtFrom === "selected" || evtFrom ==
@@ -90,7 +100,12 @@ var circularMenu = class circularMenu {
       var x = this.evt.x;
       var y = this.evt.y;
       let dbId = data[0].dbIdArray[0];
-      let myNode = await bimobjService.getBIMObject(data[0].dbIdArray[0],
+      // il faut récupérer les BIMObject selected
+      // let myNode = await window.spinal.BimObjectService();
+      // console.log("hello bimobject");
+      // console.log(data)
+      let myNode = await window.spinal.BimObjectService.getBIMObject(data[0]
+        .dbIdArray[0],
         data[0].model);
       if (myNode != undefined) {
         let objContextMenuService = {
@@ -141,7 +156,7 @@ var circularMenu = class circularMenu {
       });
   }
   mount() {
-    bimobjService.getGraph();
+    // bimobjService.getGraph();
   }
   open(buttonList, x, y, objContextMenuService) {
     if (this.close() == false) {
